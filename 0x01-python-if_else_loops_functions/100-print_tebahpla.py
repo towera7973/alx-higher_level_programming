a57847a9eccdca0b2bc5c2c5111a882b3f58e973
towera7973 <@towera7973@gmail.com@>
2024-01-03 02:39:54 -0800

    reverse alphq letters in lower and upper case

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-for i in range(122, 64, -1):
-    print(f"{chr(i)}{chr(i - 32)}", end="")
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        a = i
+    else:
+        a = i - 32
+    print("{}".format(chr(a)), end="")
+
 
