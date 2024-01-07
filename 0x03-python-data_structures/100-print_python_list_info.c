95abc3de580282a245d66a16a7ae35d7cdc145d6
towera7973 <@towera7973@gmail.com@>
2024-01-07 11:22:41 -0800

    all

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,29 +1,26 @@
-#include <stdio.h>
 #include <Python.h>
+#include <stdio.h>
 #include <object.h>
 #include <listobject.h>
+
 /**
- * print_python_list_info - shows size,allocated space and type of elements of  Python lists pointed by c object(p) in c language
- * @p: object
- *
- * Return: 0
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p: python object.
  */
 void print_python_list_info(PyObject *p)
 {
-	long int list_size, space_allocated, i;
+	int size;
+	int allocated;
+	int i;
 
-	i = 0;
-	PyObject *container;
+	size = Py_SIZE(p);
+	allocated = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", allocated);
 
-	list_size = PyList_Size(p);
-	space_allocated = ((PyListObject *)p)->allocated;
-
-	printf("[*] Size of the Python List = %ld\n", list_size);
-	printf("[*] Allocated = %ld\n", space_allocated);
-	while (i < list_size)
+	for (i = 0; i < size; i++)
 	{
-		container = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(container)->tp_name);
-		i++;
+		printf("Element %d: ", i);
+		printf("%s\n", ((PyList_GetItem(p, i))->ob_type)->tp_name);
 	}
 }
