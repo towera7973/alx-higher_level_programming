4ea3960eb4d4f26f723a5cb955ec4b9c5459ffb8
towera7973 <@towera7973@gmail.com@>
2024-01-10 12:36:20 -0800

    fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """Replace number on replace on position search in a list """
-    new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-            new_list=my_list
-    return (new_list)
+    return([replace if number == search else number for number in my_list])
