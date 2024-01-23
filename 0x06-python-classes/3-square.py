2623f94688c3340f5cc81edaca79ce8059075344
towera7973 <towera7973@gmail.com>
2024-01-23 03:46:49 -0800

    quick fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
+"""creates class Square with
+private instance attribute size and public instance method"""
+
+
 class Square:
-    """
-    a class Square that defines a square by: (based on 2-square.py)
-    """
+    """defines class with instantiated and validated private instance attribute
+and public instance method."""
+
     def __init__(self, size=0):
-        """Creates new instances of square.
-        """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """Calculates the area of square.
-        """
-        return self.__size * 2
+        """calculates and returns the  square area"""
+        return(self.__size * self.__size)
