40aa61c98f0a553fa3f20e84300b45c4d5503ec7
towera7973 <towera7973@gmail.com>
2024-01-23 03:02:42 -0800

    first 3

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,4 @@
+class Square:
+    def __init__(self,size):
+        self.__size=size
+
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class that defines properties of square by: (based on 2-square.py).
+    """
+    def __init__(self, size=0):
+        """Creates new instances of square.
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the area of square.
+        """
+        return self.__size ** 2
