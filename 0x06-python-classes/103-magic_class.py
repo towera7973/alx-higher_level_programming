3a234fdd1ddabd7c232d4e7872d7dab91352e4cd
towera7973 <towera7973@gmail.com>
2024-01-24 14:18:40 -0800

    fix

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,2 +1,19 @@
-#!/usr/bin/python3
-#!/usr/bin/python3
+import math
+
+class MagicClass:
+    def __init__(self, radius):
+        self.__radius = 0  # Initialize __radius attribute
+
+        # Check if radius is an integer or float
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        # Assign radius to __radius attribute
+        self.__radius = radius
+
+    def area(self):
+        return 2 ** self.__radius * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
+
