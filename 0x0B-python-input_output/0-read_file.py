55878d961d71bd7c8f1b5ba120d8ff63210c3d12
towera7973 <towera7973@gmail.com>
2024-02-06 07:02:16 -0800

    fix 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""defines a test fie reading function """
 def read_file(filename=""):
     """function that reads a text file and prints it"""
 
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         text = f.read()
         print(text, end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,5 +2,5 @@
 def write_file(filename="", text=""):
     """a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
 
-    with open(filename, 'w') as f:
-        return f.write(text)
+    with open(filename, 'w') as file:
+        return file.write(text)
