755fad431f1f0134d1996924561b01653388aebc
towera7973 <towera7973@gmail.com>
2024-02-06 10:31:28 -0800

    from json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""Defines a string-to-JSON function."""
 
 import json
-
 
 def to_json_string(my_obj):
     """this func returns JSON representation of an object(string)"""
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""loads from object to json"""
 import json
 
 
