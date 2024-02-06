0ca5de6629b2595b8f99a5ae6676be039e4d6079
towera7973 <towera7973@gmail.com>
2024-02-06 06:54:44 -0800

    first 6

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
+
+    with open(filename, 'w') as f:
+        return f.write(text)
