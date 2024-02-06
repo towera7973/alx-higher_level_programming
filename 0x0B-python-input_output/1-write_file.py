c82e016e9bcf16d49289a6821f778a4f4bfd7657
towera7973 <towera7973@gmail.com>
2024-02-06 09:50:50 -0800

    add utf 8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 def write_file(filename="", text=""):
     """a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
 
-    with open(filename,'w', encoding="utf-8")) as file:
+    with open(filename,'w', encoding="utf-8") as file:
         return file.write(text)
