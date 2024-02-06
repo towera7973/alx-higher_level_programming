a1abff232a3f85bd6c41612e42fe2aec34768593
towera7973 <towera7973@gmail.com>
2024-02-06 09:48:35 -0800

    add utf 8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""write file"""
+
 def write_file(filename="", text=""):
     """a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
 
-    with open(filename, 'w') as file:
+    with open(filename,'w', encoding="utf-8")) as file:
         return file.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,5 +3,5 @@ def append_write(filename="", text=""):
     """function that appends a string at the end of a text file and return
     the number of characters added"""
 
-    with open(filename, 'a') as f:
+    with open(filename, encoding="utf-8"),'a') as f:
         return f.write(text)
