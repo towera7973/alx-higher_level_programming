23534061a23d8e247d03a4c8eae873efbca19f49
towera7973 <towera7973@gmail.com>
2024-03-14 19:22:07 -0700

    middle one

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- List all cities of CA that can be found in db 'hbtn_0d_usa'
+-- 'states' table contains only one record where 'name = California'
+-- but 'id' cant be different
+-- Results must be sorted in ascending order by 'cities.id'
+-- Not allowed to use JOIN keyword
+SELECT cities.id, name FROM cities WHERE state_id =
+(SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- List all cities in db 'hbtn_0d_usa'
+-- Each record should display cities.id, cities.name, and states.name
+--Results must be sorted in ascending order by cities.id
+-- Can only use SELECT statement once
+SELECT cities.id, cities.name, states.name
+FROM states
+INNER JOIN cities
+ON states.id = cities.state_id;
