07d39195bff437e099bd6fcb65aa56cc142ea595
towera7973 <towera7973@gmail.com>
2024-04-10 12:25:06 -0700

    all

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
-/* Write a class Square that defines a square and inherits from Square of 5-square.js */
-const sq1 = require('./5-square');
-
-class Square extends sq1 {
-}
-
-/* Create an instance method called charPrint(c) that prints the rectangle using the character c */
-Square.prototype.charPrint = function (c) {
-  /* If c is undefined, use the character X */
-  if (!c) {
-    c = 'X';
-  }
-  for (let i = 0; i < this.width; i++) {
-    console.log(c.repeat(this.width));
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
   }
 };
-
-module.exports = Square;
